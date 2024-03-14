d1b2ab934610e0aa36d67f212a05fecca5a374c1
boussouar ismail <123643279+boussouarismail@users.noreply.github.com>
2024-03-14 13:36:36 +0100

    first in b1 branch

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ function func1(parameter){
   return "the first line";
 }
 
-function func2(parameter){
-  return "THE SECOND LINE";
-}
 
 function func3(parameter){
   return "the third line";
+}
+
+function func2(params) {
+  return "the second line";
 }
 
 console.log(func1());
