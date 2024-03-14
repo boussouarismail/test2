6017baf9519ff29a9bbb94fab212b05e30eda9a6
boussouarismail <123643279+boussouarismail@users.noreply.github.com>
2024-03-14 13:39:26 +0100

    third commit in main branch

--- index.js
+++ index.js
@@ -2,4 +2,12 @@ function func1(parameter){
   return "the first line";
 }
 
+function func2(parameter){
+  return "THE SECOND LINE";
+}
+
+function func3(parameter){
+  return "the third line";
+}
+
 console.log(func1());
